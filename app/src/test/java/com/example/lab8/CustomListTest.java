d9fb477dc98601bd57df3fc6527da56d83b95222
Raffle703 <rtabraha@ualberta.ca>
2023-03-09 16:46:29 -0700

    Add testAdd()

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import static java.security.AccessController.getContext;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
-
-
+    @Test
+    void testAdd() {
+        ArrayList<City> cityDataList = new ArrayList<>();
+        CustomList cl = new CustomList(null, cityDataList);
+        City city = new City("Regina", "SK");
+        cl.add(city);
+    }
 }
